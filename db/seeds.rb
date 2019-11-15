6bed37e17e5636f5603fcd05e2e65d9629d45924
Vincent Tso <vincent11102@hotmail.com>
2019-11-15 20:44:12 +1100

    attempting to seed cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,60 @@ Ingredient.destroy_all
 puts 'Clearing cocktails...'
 Cocktail.destroy_all
 
-puts 'Seeding ingredients...'
+puts 'Seeding cocktails...'
+
+cocktails = [
+  {
+    name: 'Mojito',
+    description: 'The best mojito you\'ll ever find. What a delight!',
+    photo: '../../app/assets/images/mojito.jpeg'
+  },
+  {
+    name: 'Cuba Libre',
+    description: 'Want an exotic experience? This Cuba Libre will do just the trick!',
+    photo: 'cuba_libre.jpeg'
+  },
+  {
+    name: 'Sex On The Beach',
+    description: 'Spice and excitement is just what the sex on the beach delivers!',
+    photo: 'sex_on_the_beach'
+  },
+  {
+    name: 'Rhubarb Gin',
+    description: 'Only the best for cocktail experts.',
+    photo: 'rhubarb_gin.jpeg'
+  },
+  {
+    name: 'Pina Colada',
+    description: 'Nothing more delightful than a well made pina colada!',
+    photo: 'pina_colada.jpeg'
+  },
+  {
+    name: 'Martini',
+    description: 'Class and taste.',
+    photo: 'martini.jpeg'
+  },
+  {
+    name: 'Bloody Mary',
+    description: 'Exciting! Red, red and.. Red!',
+    photo: 'bloody_mary.jpeg'
+  },
+  {
+    name: 'Margarita',
+    description: 'The best things in life are tangy!',
+    photo: 'margarita.jpeg'
+  }
+]
+
+cocktails.each do |cocktail|
+  upload = Cloudinary::Uploader.upload(cocktail[:photo], :crop => "limit")
+  new_cocktail = Cocktail.new(cocktail)
+  new_cocktail.photo = upload
+end
+
+puts 'Done! Seeding ingredients...'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-
-doses_units = %w[drops pinch ml cup tsp tbsp]
 
 JSON.parse(URI.open(url).read)['drinks'].each do |ingredients|
   Ingredient.create(name: ingredients['strIngredient1'])
