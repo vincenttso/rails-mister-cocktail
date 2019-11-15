c951f27a66cc575f0ece3abd6694f9430b82ca57
Vincent Tso <vincent11102@hotmail.com>
2019-11-15 15:04:25 +1100

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  root to: 'cocktails#index'
+  resources :cocktails, except: [:index] do
     resources :doses, only: %i[new create]
   end
   resources :doses, only: %i[edit update destroy]
