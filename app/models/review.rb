85edbab06c74950a08daeb1c4535fb72a61b77b3
Vincent Tso <vincent11102@hotmail.com>
2019-11-17 23:27:13 +1100

    Adjusted validations on review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
 
-  validates :content, :rating, :user, presence: true
+  validates :cocktail_id, :content, :rating, :user, presence: true
   validates :content, :user, format: { with: /\w+/ }
-  validates :user, uniqueness: true
+  validates :cocktail_id, :user, uniqueness: true
   validates :rating, numericality: { only_integer: true }, inclusion: { in: [1, 2, 3, 4, 5] }
 end
